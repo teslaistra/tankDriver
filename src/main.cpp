e52f3b9627ac0a32fae80d06006ad89e5a7546b1
Artem Evseev <a.ewseew@walli.com>
2020-02-25 01:16:19 +0300

    Control for tank by Serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,83 @@
 #include <Arduino.h>
-
 #include <L298N.h>
+#include "MegunoLink.h"
+#include "CommandHandler.h"
 
-//pin definition
 #define EN 9
-#define IN1 8
-#define IN2 7
+#define IN1 3
+#define IN2 4
+#define IN3 5
+#define IN4 6
 
-//create a motor instance
-L298N motor(EN, IN1, IN2);
+#define TURN_LEFT 4
+#define TURN_RIGHT 3
+#define STOP 2
+#define FORWARD 1
+#define BACKWARD 0
+ 
+CommandHandler<> SerialCommandHandler;
+
+L298N motorLeft(EN, IN1, IN2);
+L298N motorRight(EN, IN4, IN3);
+int way = STOP;
+int time = 0;
+uint32_t setTime;
+
+void Cmd_Move(CommandParameter &Parameters)
+{
+  way = Parameters.NextParameterAsInteger(way);
+  time = Parameters.NextParameterAsInteger(time);
+  setTime = millis();
+}
+
+void Cmd_Unknown()
+{
+  Serial.println(F("I don't understand"));
+}
 
 void setup() {
-  Serial.begin(9600);
-  motor.setSpeed(255); // an integer between 0 and 255
+  SerialCommandHandler.AddCommand(F("Move"), Cmd_Move);
+  SerialCommandHandler.SetDefaultHandler(Cmd_Unknown);
 
+  Serial.begin(9600);
+  motorLeft.setSpeed(255);
+  motorRight.setSpeed(255);
 }
 
 void loop() {
+  SerialCommandHandler.Process();
 
-  //tell the motor to go forward (may depend by your wiring)
-  motor.forward();
-
-  //print the motor satus in the serial monitor
-  Serial.print("Is moving = ");
-  Serial.println(motor.isMoving());
-
-  delay(3000);
-
-  //stop running
-  motor.stop();
-
-  Serial.print("Is moving = ");
-  Serial.println(motor.isMoving());
-
-  delay(3000);
-
-  //change the initial speed
-  motor.setSpeed(100);
-
-  //tell the motor to go back (may depend by your wiring)
-  motor.backward();
-
-  Serial.print("Is moving = ");
-  Serial.println(motor.isMoving());
-
-  delay(3000);
-
-  //stop running
-  motor.stop();
-
-  Serial.print("Is moving = ");
-  Serial.println(motor.isMoving());
-
-  //change the initial speed
-  motor.setSpeed(255);
-
-  Serial.print("Get new speed = ");
-  Serial.println(motor.getSpeed());
-
-  delay(3000);
+  uint32_t uNow = millis();
+  if (way != STOP) {
+    if (uNow < setTime + (uint32_t)time ) {
+      switch (way) {
+        case STOP:
+          motorLeft.stop();
+          motorRight.stop();
+          break;
+        case FORWARD:
+          motorLeft.forward();
+          motorRight.forward();
+          break;
+        case BACKWARD:
+          motorLeft.backward();
+          motorRight.backward();
+          break;
+        case TURN_RIGHT:
+          motorLeft.forward();
+          motorRight.backward();
+          break;
+        case TURN_LEFT:
+          motorLeft.backward();
+          motorRight.forward();
+          break;
+        default:
+          break;
+      }
+    } else {
+      way = STOP;
+      motorRight.stop();
+      motorLeft.stop();
+    }
+  }
 }
